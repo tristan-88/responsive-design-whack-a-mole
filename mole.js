2714f9f1b9b71782115a4773119f127fcb3c8c3a
Tristan San Juan <tristansj88@gmail.com>
2021-02-10 14:58:07 -0700

    finished phase 2 step2

--- mole.js
+++ mole.js
@@ -1,8 +1,39 @@
-window.addEventListener("DOMContentLoaded", () => {
-  setInterval(() => {
-    const moleHeads = document.querySelectorAll(".wgs__mole-head");
-    for (let moleHead of moleHeads) {
-      moleHead.classList.toggle("wgs__mole-head--hidden");
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    
+  
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}  
+    function popUpRandomMole() {
+        const moleHeads = document.querySelectorAll(".wgs__mole-head");
+     
+        
+        console.log(moleHeads)
+        const randomNum = getRandomIntInclusive(0, 7)
+        let moleHeadRandom = moleHeads[randomNum]
+        console.log(moleHeadRandom)
+        moleHeadRandom.classList.remove("wgs__mole-head--hidden")
+    
+        setTimeout(() => {
+            hideMole(moleHeadRandom);
+        },1000)
     }
-  }, 1000);
+
+    function hideMole(mole) {
+        mole.classList.add("wgs__mole-head--hidden");
+        setTimeout(() => {
+            popUpRandomMole()
+        },1000)
+    }
+
+  setTimeout(() => {
+    popUpRandomMole();
+  }, 0);
+    
+    
 });
